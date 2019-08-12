6870a7dea7469470d5b770a97fcba38bd4d36123
jslodkowicz <jan.slod@gmail.com>
2019-08-12 14:47:09 +0200

    added pagination feature

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['jan.slod@gmail.com']
+    POSTS_PER_PAGE = 25
